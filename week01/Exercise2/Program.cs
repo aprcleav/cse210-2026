c7a527b65b67b8843c1a61d45d9be4a92d1fc6cd
aprcleav <aprcleaver@gmail.com>
2026-01-06 13:00:34 -0800

    Finished W1 Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? (example: 90) ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+        string letter = "";
+        string sign;
+        int lastDigit = grade % 10;
+
+        // Finds letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        // Determines if letter grade is + or -
+        if (lastDigit >= 7)
+        {
+            if (letter == "A" || letter == "F")
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "+";
+            }
+        }
+        else if (lastDigit < 3)
+        {
+            if (letter == "F")
+            {
+                sign = "";
+            }
+            else
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Displays pass or fail message
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed this time, but keep trying. You can do it!");
+        }
+
+        // Displays final letter grade with sign
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
     }
 }
