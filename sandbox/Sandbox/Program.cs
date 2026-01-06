896fd8d89323c55c94e43dbef62177d542a38cc6
aprcleav <aprcleaver@gmail.com>
2026-01-06 11:54:50 -0800

    Completed W1 Exercise 1.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is in C#.");
+
+        int number = 5;
+
+        if (number > 3)
+        {
+            Console.WriteLine("greater");
+        }
+
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}.");
+        
     }
 }
