413c69c23c3b12edbf154305e2a2164f147d70de
aprcleav <aprcleaver@gmail.com>
2026-01-19 14:20:07 -0800

    Created stubs for scripture memorizer program

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "Hello"; //placeholder
+    }
+    
+    public bool IsCompletelyHiddden()
+    {
+        return true; //placeholder
+    }
+}
