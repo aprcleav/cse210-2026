a63d4c0ed7f084c1648bd64bee8a3963cb9802f0
aprcleav <aprcleaver@gmail.com>
2026-01-31 10:47:52 -0800

    updated variables

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -3,7 +3,7 @@ public class MathAssignment : Assignment
     private string _textbookSection;
     private string _problems;
 
-    public MathAssignment(string name, string topic, string textbookSection, string problems) : base(name, topic)
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
     {
         _textbookSection = textbookSection;
         _problems = problems;
